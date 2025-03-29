b163129f7f3686c3b53cac33d358fcdc6298bb74
Venkata-Giridhar-Garikipati <venkatagiridhargarikipati@gmail.com>
2025-03-29 10:57:48 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+function App() {
+  const data = [
+    { id: 1, name: 'Venkata Giridhar', age: 20 },
+    { id: 2, name: 'Hemanth', age: 20 },
+    { id: 3, name: 'Vinay', age: 20 },
+    { id: 4, name: 'Yaswanth', age: 20 ,color: 'yellow'}, 
+  ];
+  return (
+    <div className="App">
+      <table border="1" align="center">
+        <caption>Data Table</caption>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+        {data.map((item) => (
+          <tr key={item.id}
+           id={item.color} style={{ backgroundColor: item.color }}>
+               <td id="red">{item.id}</td>
+               <td id="yellow" >{item.name}</td>
+               <td id ="yellow">{item.age}</td>
+            
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
